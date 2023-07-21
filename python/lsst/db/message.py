3facbe5660d7d9589a1c7436b0e2eb276f9e6d45
cpio <cgarcia@ifae.es>
2023-07-21 16:24:21 +0200

    Solving minor bugs

--- python/lsst/db/message.py
+++ python/lsst/db/message.py
@@ -9,17 +9,17 @@ class Message(Base):
     __tablename__ = 'message'
 
     date_added = Column(DateTime(), nullable=False),
-    date_invalidated = Column(DateTime(), nullable=True),
+    date_invalidated = Column(DateTime(), nullable=True)
     day_obs = Column(Integer(), nullable=False)
     exposure_flag = Column(String(), nullable=False)
-    id = Column(UUID(), nullable=False),
+    id = Column(UUID(), nullable=False, primary_key=True)
     instrument = Column(String(), nullable=True)
     is_human = Column(Boolean(), nullable=False)
     is_valid = Column(Boolean(), nullable=False)
-    level = Column(Integer(), nullable=False),
+    level = Column(Integer(), nullable=False)
     message_text = Column(String(), nullable=False)
     obs_id = Column(String(), nullable=False)
-    parent_id = Column(UUID(), nullable=True),
+    parent_id = Column(UUID(), nullable=True)
     seq_num = Column(Integer(), nullable=False)
     site_id = Column(String(), nullable=True)
     tags = Column(ARRAY(String()), nullable=False)
